2b0efec99fa08b5faa7fd336386ec7ef1d26ed13
KoichiHirai <i.remember_tt@icloud.com>
2024-04-06 08:20:15 +0900

    implemented station5

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import paginationReducer from './reducers';
+import paginationReducer from '../reducers/reducer';
 
 const store = configureStore({
   reducer: {
     pagination: paginationReducer,
   },
 });
+
+// import { paginationReducer, dataReducer } from '../reducers/reducer';
+
+// const store = configureStore({
+//   reducer: {
+//     pagination: paginationReducer,
+//     data: dataReducer,
+//   },
+// });
 
 export default store;
